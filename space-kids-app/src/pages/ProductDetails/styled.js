de50aa357d6e4e6b35d9ad472e3defa3ca8c3c14
suuzanemoura <[suuzanemoura@gmail.com]>
2023-02-09 09:42:05 -0300

    Página de detalhes do produto com responsividade feita

--- space-kids-app/src/pages/ProductDetails/styled.js
+++ space-kids-app/src/pages/ProductDetails/styled.js
@@ -17,6 +17,21 @@ export const Card = styled.div`
   border-radius: 1.5rem;
   padding: 1rem;
   gap: 4rem;
+
+  @media screen and (max-width: 1280px) {
+    width: 60rem;
+  }
+
+  @media screen and (max-width: 1024px) {
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  @media screen and (max-width: 580px) {
+    width: 45rem;
+  }
 `;
 
 export const ProductInfos = styled.div`
@@ -25,17 +40,49 @@ export const ProductInfos = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1rem;
+  order: 1;
+
+  @media screen and (max-width: 1280px) {
+    width: 30rem;
+  }
+
+  @media screen and (max-width: 1024px) {
+    order: 2;
+  }
+
+  @media screen and (max-width: 580px) {
+    width: 20rem;
+  }
+
+  @media screen and (max-width: 375px) {
+    width: 15rem;
+  }
 
   h1 {
     color: #ff6e6c;
     font-size: 3rem;
     line-height: 1rem;
     margin-top: 1rem;
+
+    @media screen and (max-width: 580px) {
+      font-size: 2.5rem;
+      margin-top: 0.5rem;
+    }
   }
 
   h2 {
     font-size: 2.7rem;
     line-height: 3.2rem;
+
+    @media screen and (max-width: 1024px) {
+      font-size: 2.3rem;
+      line-height: 2.8rem;
+    }
+
+    @media screen and (max-width: 580px) {
+      font-size: 1.6rem;
+      line-height: 2rem;
+    }
   }
 `;
 
@@ -44,6 +91,26 @@ export const ProductImage = styled.img`
   height: 30rem;
   object-fit: contain;
   border-radius: 1rem;
+  order: 2;
+
+  @media screen and (max-width: 1280px) {
+    width: 26rem;
+    height: 26rem;
+  }
+
+  @media screen and (max-width: 1024px) {
+    order: 1;
+  }
+
+  @media screen and (max-width: 580px) {
+    width: 18rem;
+    height: 18rem;
+  }
+
+  @media screen and (max-width: 375px) {
+    width: 15rem;
+    height: 15rem;
+  }
 `;
 
 export const Button = styled.button`
